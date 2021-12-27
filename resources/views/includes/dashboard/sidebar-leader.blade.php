d3a165c8edf7d1ff21883c6567518b2259182d16
Adrian Mulyawan <adrianmulyawan666@gmail.com>
2021-12-27 13:26:53 +0700

    edit sidebar pimpinan + buat controller pengajuan izin

--- resources/views/includes/dashboard/sidebar-leader.blade.php
+++ resources/views/includes/dashboard/sidebar-leader.blade.php
@@ -8,31 +8,31 @@
     </div>
     <!-- Menu Sidebar -->
     <div class="list-group list-group-flush">
-        <a href="../pimpinan/dashboard.html" class="list-group-item list-group-item-action active">
+        <a href="#" class="list-group-item list-group-item-action">
             Dashboard
         </a>
-        <a href="../pimpinan/dashboardPengajuan.html" class="list-group-item list-group-item-action">
+        <a href="#" class="list-group-item list-group-item-action">
             Pengajuan Izin Masuk
         </a>
-        <a href="../pimpinan/dashboardPembayaran.html" class="list-group-item list-group-item-action">
+        <a href="#" class="list-group-item list-group-item-action">
             Pembayaran Retribusi
         </a>
-        <a href="../pimpinan/dashboardSuratIzinMasuk.html" class="list-group-item list-group-item-action">
+        <a href="#" class="list-group-item list-group-item-action">
             Surat Izin Masuk
         </a>
-        <a href="../pimpinan/dashboardPengajuanBulanan.html" class="list-group-item list-group-item-action">
+        <a href="#" class="list-group-item list-group-item-action">
             Rekapan Pengajuan Bulanan
         </a>
-        <a href="../pimpinan/dashboardMasukan.html" class="list-group-item list-group-item-action">
+        <a href="#" class="list-group-item list-group-item-action">
             Form Kepuasan & Kekurangan
         </a>
-        <a href="../pimpinan/dashboardKeluarMasukPengunjung.html" class="list-group-item list-group-item-action">
+        <a href="#" class="list-group-item list-group-item-action">
             Data Keluar Masuk Pengunjung
         </a>
-        <a href="../pimpinan/dashboardSetting.html" class="list-group-item list-group-item-action">
+        <a href="#" class="list-group-item list-group-item-action">
             Pengaturan Akun
         </a>
-        <a href="../index.html" class="list-group-item list-group-item-action mt-5">
+        <a href="#" class="list-group-item list-group-item-action mt-5">
             Keluar
         </a>
     </div>
