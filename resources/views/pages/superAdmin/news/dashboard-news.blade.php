08e1de74fa32c09f6622d61ebc481ea148d666c4
Adrian Mulyawan <adrianmulyawan666@gmail.com>
2022-01-07 11:14:56 +0700

    tambah pagination berita

--- resources/views/pages/superAdmin/news/dashboard-news.blade.php
+++ resources/views/pages/superAdmin/news/dashboard-news.blade.php
@@ -77,16 +77,7 @@
                     </div>
                     <!-- Pagination -->
                     <div class="row justify-content-end mr-2">
-                        <nav aria-label="Page navigation example">
-                            <ul class="pagination">
-                              <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-                              <li class="page-item"><a class="page-link" href="#">1</a></li>
-                              <li class="page-item"><a class="page-link" href="#">2</a></li>
-                              <li class="page-item"><a class="page-link" href="#">3</a></li>
-                              <li class="page-item"><a class="page-link" href="#">Next</a></li>
-                            </ul>
-                        </nav>
-                    </div>
+                        {{ $items->links() }}
                 </div>
             </div>
         </div>
