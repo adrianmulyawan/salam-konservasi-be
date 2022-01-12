e46f16729c5669353bfe5776ddd7bfc84eaf384f
Adrian Mulyawan <adrianmulyawan666@gmail.com>
2022-01-12 19:30:35 +0700

    Implentasi: tampilkan seluruh data kawasan dan detail kawasan

--- app/Http/Controllers/ConservationAreaController.php
+++ app/Http/Controllers/ConservationAreaController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ConservationArea;
 use Illuminate\Http\Request;
 
 class ConservationAreaController extends Controller
@@ -13,6 +14,7 @@ class ConservationAreaController extends Controller
      */
     public function index()
     {
-        return view('pages.conservation-area');
+        $items = ConservationArea::with(['galleries'])->paginate(5);
+        return view('pages.conservation-area', compact('items'));
     }
 }
--- resources/views/pages/conservation-area.blade.php
+++ resources/views/pages/conservation-area.blade.php
@@ -18,81 +18,27 @@
         <div class="section-conservation-area-list">
             <div class="container">
                 <div class="section-conservation-area row justify-content-start">
-                    <div class="col-sm-6 col-md-4 col-lg-4 mt-3" data-aos="fade-up" data-aos-delay="100">
-                        <div class="card">
-                            <img src="frontend/images/daftar-kawasan-img/kawasan1.png" class="card-img-top">
-                            <div class="card-body">
-                                <h5 class="card-title">KKPD Pulau Randayan</h5>
-                                <p class="card-location">Lokasi: Kabupaten Bengkayang</p>
-                                <p class="card-text">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac turpis egestas sed tempus urna et pharetra.
-                                </p>
-                                <a href="detailsConservation.html" class="btn btn-detail-conservation mt-3 px-5">
-                                    Detail
-                                </a>
+                    <?php $counter = 0; ?>
+                    @foreach ($items as $item)
+                        <div class="col-sm-6 col-md-4 col-lg-4 mt-3" data-aos="fade-up" data-aos-delay="{{ $counter += 100; }}">
+                            <div class="card">
+                                <img src="{{ Storage::url($item->galleries->first()->photo) }}" class="card-img-top">
+                                <div class="card-body">
+                                    <h5 class="card-title">{{ $item->name }}</h5>
+                                    <p class="card-location">Lokasi: {{ $item->location }}</p>
+                                    <p class="card-text">
+                                        {!! Str::limit($item->description, 200, '.') !!}
+                                    </p>
+                                    <a href="{{ route('detailConservation', $item->slug) }}" class="btn btn-detail-conservation mt-3 px-5">
+                                        Detail
+                                    </a>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-sm-6 col-md-4 col-lg-4 mt-3" data-aos="fade-up" data-aos-delay="200">
-                        <div class="card">
-                            <img src="frontend/images/daftar-kawasan-img/kawasan2.png" class="card-img-top">
-                            <div class="card-body">
-                                <h5 class="card-title">KKPD Paloh</h5>
-                                <p class="card-location">Lokasi: Kabupaten Sambas</p>
-                                <p class="card-text">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac turpis egestas sed tempus urna et pharetra.
-                                </p>
-                                <a href="detailsConservation.html" class="btn btn-detail-conservation mt-3 px-5">
-                                    Detail
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-6 col-md-4 col-lg-4 mt-3" data-aos="fade-up" data-aos-delay="300">
-                        <div class="card">
-                            <img src="frontend/images/daftar-kawasan-img/kawasan3.png" class="card-img-top">
-                            <div class="card-body">
-                                <h5 class="card-title">KKPD Kubu Raya</h5>
-                                <p class="card-location">Lokasi: Kabupaten Kubu Raya</p>
-                                <p class="card-text">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac turpis egestas sed tempus urna et pharetra.
-                                </p>
-                                <a href="detailsConservation.html" class="btn btn-detail-conservation mt-3 px-5">
-                                    Detail
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-6 col-md-4 col-lg-4 mt-3" data-aos="fade-up" data-aos-delay="400">
-                        <div class="card">
-                            <img src="frontend/images/daftar-kawasan-img/kawasan4.png" class="card-img-top">
-                            <div class="card-body">
-                                <h5 class="card-title">KKPD Kendawangan</h5>
-                                <p class="card-location">Lokasi: Kabupaten Kayong Utara</p>
-                                <p class="card-text">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac turpis egestas sed tempus urna et pharetra.
-                                </p>
-                                <a href="detailsConservation.html" class="btn btn-detail-conservation mt-3 px-5">
-                                    Detail
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-6 col-md-4 col-lg-4 mt-3" data-aos="fade-up" data-aos-delay="500">
-                        <div class="card">
-                            <img src="frontend/images/daftar-kawasan-img/kawasan5.png" class="card-img-top">
-                            <div class="card-body">
-                                <h5 class="card-title">KKPD Ketapang</h5>
-                                <p class="card-location">Lokasi: Kabupaten Ketapang</p>
-                                <p class="card-text">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac turpis egestas sed tempus urna et pharetra.
-                                </p>
-                                <a href="detailsConservation.html" class="btn btn-detail-conservation mt-3 px-5">
-                                    Detail
-                                </a>
-                            </div>
-                        </div>
-                    </div>
+                    @endforeach
+                </div>
+                <div class="row justify-content-end mt-3 mr-0">
+                    {{ $items->links() }}
                 </div>
             </div>
         </div>
