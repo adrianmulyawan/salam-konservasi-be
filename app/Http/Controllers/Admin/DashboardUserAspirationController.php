9915934ee3edd74175e30441639396cb8cd10460
Adrian Mulyawan <adrianmulyawan666@gmail.com>
2022-01-18 15:26:41 +0700

    implementasi: menampilkan seluruh data aspirasi

--- app/Http/Controllers/Admin/DashboardUserAspirationController.php
+++ app/Http/Controllers/Admin/DashboardUserAspirationController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\UserAspiration;
 use Illuminate\Http\Request;
 
 class DashboardUserAspirationController extends Controller
 {
     public function index()
     {
-        return view('pages.superAdmin.userAspiration.dashboard-user-aspiration');
+        $forms = UserAspiration::orderBy('created_at', 'DESC')->paginate(10);
+        return view('pages.superAdmin.userAspiration.dashboard-user-aspiration', compact('forms'));
     }
 
     public function detail($id)
--- resources/views/pages/superAdmin/userAspiration/dashboard-user-aspiration.blade.php
+++ resources/views/pages/superAdmin/userAspiration/dashboard-user-aspiration.blade.php
@@ -30,132 +30,35 @@
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        <tr>
-                                            <td class="text-center">1</td>
-                                            <td class="text-center">Rachel Venya</td>
-                                            <td>Penjaga Kawasan Tidak Ramah</td>
-                                            <td>
-                                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis neque odio quo repellat obcaecati.
-                                            </td>
-                                            <td>
-                                                <a href="#" class="btn btn-sim-kawasan mt-auto px-4">Detail</a>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td class="text-center">2</td>
-                                            <td class="text-center">Muhammad Hakaman Athhar</td>
-                                            <td>Penjaga Kawasan Tidak Ramah</td>
-                                            <td>
-                                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis neque odio quo repellat obcaecati.
-                                            </td>
-                                            <td class="text-center">
-                                                <a href="#" class="btn btn-sim-kawasan mt-auto px-4">Detail</a>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td class="text-center">3</td>
-                                            <td class="text-center">Revianto</td>
-                                            <td>Penjaga Kawasan Tidak Ramah</td>
-                                            <td>
-                                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis neque odio quo repellat obcaecati.
-                                            </td>
-                                            <td class="text-center">
-                                                <a href="#" class="btn btn-sim-kawasan mt-auto px-4">Detail</a>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td class="text-center">4</td>
-                                            <td class="text-center">Badrul</td>
-                                            <td>Penjaga Kawasan Tidak Ramah</td>
-                                            <td>
-                                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis neque odio quo repellat obcaecati.
-                                            </td>
-                                            <td class="text-center">
-                                                <a href="#" class="btn btn-sim-kawasan mt-auto px-4">Detail</a>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td class="text-center">5</td>
-                                            <td class="text-center">Maman Abdurahman</td>
-                                            <td>Penjaga Kawasan Tidak Ramah</td>
-                                            <td>
-                                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis neque odio quo repellat obcaecati.
-                                            </td>
-                                            <td class="text-center">
-                                                <a href="#" class="btn btn-sim-kawasan mt-auto px-4">Detail</a>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td class="text-center">6</td>
-                                            <td class="text-center">Adrian Mulyawan</td>
-                                            <td>Penjaga Kawasan Tidak Ramah</td>
-                                            <td>
-                                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis neque odio quo repellat obcaecati.
-                                            </td>
-                                            <td class="text-center">
-                                                <a href="#" class="btn btn-sim-kawasan mt-auto px-4">Detail</a>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td class="text-center">7</td>
-                                            <td class="text-center">Dhafia Raisha Kamila</td>
-                                            <td>Penjaga Kawasan Tidak Ramah</td>
-                                            <td>
-                                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis neque odio quo repellat obcaecati.
-                                            </td>
-                                            <td class="text-center">
-                                                <a href="#" class="btn btn-sim-kawasan mt-auto px-4">Detail</a>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td class="text-center">8</td>
-                                            <td class="text-center">Wawan</td>
-                                            <td>Penjaga Kawasan Tidak Ramah</td>
-                                            <td>
-                                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis neque odio quo repellat obcaecati.
-                                            </td>
-                                            <td class="text-center">
-                                                <a href="#" class="btn btn-sim-kawasan mt-auto px-4">Detail</a>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td class="text-center">9</td>
-                                            <td class="text-center">Gusti</td>
-                                            <td>Penjaga Kawasan Tidak Ramah</td>
-                                            <td>
-                                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis neque odio quo repellat obcaecati.
-                                            </td>
-                                            <td class="text-center">
-                                                <a href="#" class="btn btn-sim-kawasan mt-auto px-4">Detail</a>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td class="text-center">10</td>
-                                            <td class="text-center">Chiko Jeriko</td>
-                                            <td>Penjaga Kawasan Tidak Ramah</td>
-                                            <td>
-                                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis neque odio quo repellat obcaecati.
-                                            </td>
-                                            <td class="text-center">
-                                                <a href="#" class="btn btn-sim-kawasan mt-auto px-4">Detail</a>
-                                            </td>
-                                        </tr>
+                                        <?php $counter = 0; ?>
+                                        @forelse ($forms as $form)
+                                            <?php $counter += 1 ?>
+                                            <tr>
+                                                <td class="text-center">{{ $counter }}</td>
+                                                <td class="text-center">{{ $form->name }}</td>
+                                                <td>
+                                                    {{ $form->form_title }}
+                                                </td>
+                                                <td>
+                                                    {{ Str::limit($form->form_content, 20, '...') }}
+                                                </td>
+                                                <td class="text-center">
+                                                    <a href="{{ route('AdminuserAspirationDetailAdmin', $form->id) }}" class="btn btn-sim-kawasan mt-auto px-4">Detail</a>
+                                                </td>
+                                            </tr>
+                                        @empty
+                                            <tr>
+                                                <td class="text-center" colspan="5">Belum Ada Data Apapun</td>
+                                            </tr>
+                                        @endforelse
                                     </tbody>
                                 </table>
                             </div>
                         </div>
                     </div>
                     <!-- Pagination -->
-                    <div class="row justify-content-end mr-2">
-                        <nav aria-label="Page navigation example">
-                            <ul class="pagination">
-                              <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-                              <li class="page-item"><a class="page-link" href="#">1</a></li>
-                              <li class="page-item"><a class="page-link" href="#">2</a></li>
-                              <li class="page-item"><a class="page-link" href="#">3</a></li>
-                              <li class="page-item"><a class="page-link" href="#">Next</a></li>
-                            </ul>
-                        </nav>
+                    <div class="row justify-content-end mr-1">
+                        {{ $forms->links() }}
                     </div>
                 </div>
             </div>
