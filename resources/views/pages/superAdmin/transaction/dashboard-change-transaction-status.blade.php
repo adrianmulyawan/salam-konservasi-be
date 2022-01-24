bfcd60ab90b3078a008e86ea5825c5af8af40c62
Adrian Mulyawan <adrianmulyawan666@gmail.com>
2022-01-24 20:54:39 +0700

    Menampilkan Kelola Pembayaran Admin, Merubah Status, dan Melihat Detail Pembayaran: Status Terbayar

--- resources/views/pages/superAdmin/transaction/dashboard-change-transaction-status.blade.php
+++ resources/views/pages/superAdmin/transaction/dashboard-change-transaction-status.blade.php
@@ -8,7 +8,7 @@
         <div class="container-fluid">
             <!-- 1. Heading -->
             <div class="dashboard-heading">
-                <h2 class="dashboard-transaction-title">#SALAM-89110</h2>
+                <h2 class="dashboard-transaction-title">#{{ $data->transaction_code }}</h2>
                 <!-- Breadcrumb -->
                 <div class="breadcrumb-transaction>
                     <nav aria-label="breadcrumb">
@@ -17,13 +17,13 @@
                                Pembayaran Retribusi Izin Masuk Kawasan Konservasi
                             </li>
                             <li class="breadcrumb-item active" aria-current="page">
-                                Adrian Mulyawan
+                                {{ $data->user->name }}
                             </li>
                             <li class="breadcrumb-item active" aria-current="page">
-                                Penelitian
+                                {{ $data->purpose->purpose_name }}
                             </li>
                             <li class="breadcrumb-item active" aria-current="page">
-                                KKPD Pulau Randayan
+                                {{ $data->conservation_area->name }}
                             </li>
                         </ol>
                     </nav>
@@ -33,33 +33,25 @@
                 <!-- 2. Table Data Kawasan -->
                 <div class="card card-conservation card-detail-transaction">
                     <!-- 2.1 Photo, Pemohon, Kepentingan, dll -->
-                    <div class="row">
+                    <div class="row mx-1 mt-2">
                         <div class="col-md-4">
-                            <img src="{{ url('frontend/images/detail-transaction/image.png') }}" alt="header-transaction" class="header-transaction">
+                            <img src="{{ Storage::url($data->conservation_area->galleries->first()->photo) }}" alt="header-transaction" class="header-transaction">
                         </div>
                         <div class="col-md-4">
-                            <div class="transaction-user-name mb-3">
+                            <div class="transaction-user-name mb-4">
                                 <div class="transaction-title">
                                     Nama Pemohon
                                 </div>
                                 <div class="transaction-subtitle">
-                                    Adrian Mulyawan
+                                    {{ $data->user->name }}
                                 </div>
                             </div>
-                            <div class="transaction-user-employee mb-3">
+                            <div class="transaction-user-employee mb-4">
                                 <div class="transaction-title">
                                     Kepentingan
                                 </div>
                                 <div class="transaction-subtitle">
-                                    Penelitian
-                                </div>
-                            </div>
-                            <div class="transaction-total-user mb-3">
-                                <div class="transaction-title">
-                                    Biaya Peralatan Bawaan Pengunjung
-                                </div>
-                                <div class="transaction-subtitle">
-                                    Rp 200.000,00
+                                    {{ $data->purpose->purpose_name }}
                                 </div>
                             </div>
                             <div class="transaction-date-entry">
@@ -67,33 +59,25 @@
                                     Tanggal Masuk Kawasan
                                 </div>
                                 <div class="transaction-subtitle">
-                                    12 Januari 2022
+                                    {{ \Carbon\Carbon::create($data->date_of_entry)->format('d F Y') }}
                                 </div>
                             </div>
                         </div>
                         <div class="col-md-4">
-                            <div class="transaction-destination-area mb-3">
+                            <div class="transaction-destination-area mb-4">
                                 <div class="transaction-title">
                                     Nama Kawasan Tujuan
                                 </div>
                                 <div class="transaction-subtitle">
-                                    KKPD Pulau Randayan
+                                    {{ $data->conservation_area->name }}
                                 </div>
                             </div>
-                            <div class="transaction-total mb-3">
-                                <div class="transaction-title">
-                                    Biaya Masuk Pengunjung
-                                </div>
-                                <div class="transaction-subtitle">
-                                    Rp 120.000,00
-                                </div>
-                            </div>
-                            <div class="transaction-total-day mb-3">
+                            <div class="transaction-total-day mb-4">
                                 <div class="transaction-title">
                                     Total Transksi
                                 </div>
                                 <div class="transaction-subtitle">
-                                    Rp 620.000,00
+                                    Rp {{ number_format($data->total_transaction,2,',','.') }}
                                 </div>
                             </div>
                             <div class="transaction-date-out">
@@ -101,110 +85,103 @@
                                     Tanggal Keluar Kawasan
                                 </div>
                                 <div class="transaction-subtitle">
-                                    15 Januari 2022
+                                    {{ \Carbon\Carbon::create($data->out_date)->format('d F Y') }}
                                 </div>
                             </div>
                         </div>
                     </div>
+
+
                     <!-- 2.2 Data Pengunjung -->
-                    <div class="row data-pengunjung">
+                    <div class="data-pengunjung">
                         <h5>Data Pengunjung</h5>
                     </div>
-                    <div class="row">
-                        <div class="card-body">
-                            <div class="table-responsive">
-                                <table class="table table-bordered" width="100%" cellspacing="0">
-                                    <thead>
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table table-bordered" width="100%" cellspacing="0">
+                                <thead>
+                                    <tr>
+                                        <th class="text-center">No</th>
+                                        <th class="text-center">Nama</th>
+                                        <th class="text-center">Kebangsaan</th>
+                                        <th class="text-center">No Telepon</th>
+                                        <th class="text-center">Alamat</th>
+                                        <th class="text-center">Kartu Pelajar/KTP/KK/Passport</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php $counter = 0 ?>
+                                    @foreach ($details as $detail)
                                         <tr>
-                                            <th class="text-center">No</th>
-                                            <th class="text-center">Nama</th>
-                                            <th class="text-center">Kebangsaan</th>
-                                            <th class="text-center">No Telepon</th>
-                                            <th class="text-center">Alamat</th>
-                                            <th class="text-center">Kartu Pelajar/KTP/KK/Passport</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <td class="text-center">1</td>
-                                            <td class="text-center">Adrian Mulyawan</td>
-                                            <td class="text-center">WNI</td>
-                                            <td class="text-center">082154590559</td>
-                                            <td class="text-center">Jl Sui Raya Dalam Komplek Taman Sui Raya Blok H2</td>
+                                            <td class="text-center">{{ $counter += 1 }}</td>
+                                            <td class="text-center">{{ $detail->name }}</td>
+                                            <td class="text-center">{{ $detail->citizen }}</td>
+                                            <td class="text-center">{{ $detail->phone_number }}</td>
+                                            <td class="text-center">{{ $detail->address }}</td>
                                             <td class="text-center photo-identitas">
-                                                <img src="{{ url('frontend/images/detail-transaction/KTPEL.png') }}" alt="photo-identitas">
+                                                @if ($detail->identity_image != null)
+                                                    <img src="{{ Storage::url($detail->identity_image ) }}" alt="photo-identitas">
+                                                @else
+                                                    Tidak Unggah Photo
+                                                @endif
                                             </td>
                                         </tr>
-                                        <tr>
-                                            <td class="text-center">2</td>
-                                            <td class="text-center">Raudy Alfan Suri</td>
-                                            <td class="text-center">WNI</td>
-                                            <td class="text-center">082154590559</td>
-                                            <td class="text-center">Jl Sui Raya Dalam Komplek Taman Sui Raya Blok H2</td>
-                                            <td class="text-center photo-identitas">
-                                                <img src="{{ url('frontend/images/detail-transaction/KTPEL.png') }}" alt="photo-identitas">
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
+                                    @endforeach
+                                </tbody>
+                            </table>
                         </div>
                     </div>
+                    
                     <!-- 2.3 Data Peralatan -->
-                    <div class="row data-bawaan-pengunjung">
+                    <div class="data-bawaan-pengunjung">
                         <h5>Data Bawaan Pengunjung</h5>
                     </div>
-                    <div class="row">
-                        <div class="card-body">
-                            <div class="table-responsive">
-                                <table class="table table-bordered" width="100%" cellspacing="0">
-                                    <thead>
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table table-bordered" width="100%" cellspacing="0">
+                                <thead>
+                                    <tr>
+                                        <th class="text-center">No</th>
+                                        <th class="text-center">Daftar Peralatan</th>
+                                        <th class="text-center">Jumlah Peralatan (Perbuah)</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php $counter = 0; ?>
+                                    @forelse ($equipments as $equipment)
+                                            <tr>
+                                                <td class="text-center">{{ $counter += 1 }}</td>
+                                                <td class="text-center">{{ $equipment->equipment_name }}</td>
+                                                <td class="text-center">{{ $equipment->equipment_total }} Buah</td>
+                                            </tr>
+                                    @empty
                                         <tr>
-                                            <th class="text-center">No</th>
-                                            <th class="text-center">Daftar Peralatan</th>
-                                            <th class="text-center">Jumlah Peralatan (Perbuah)</th>
+                                                <td class="text-center" colspan="3">Tidak Membawa Peralatan</td>
                                         </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <td class="text-center">1</td>
-                                            <td class="text-center">Alat Selam</td>
-                                            <td class="text-center">6 Buah</td>
-                                        </tr>
-                                        <tr>
-                                            <td class="text-center">2</td>
-                                            <td class="text-center">Kamera Bawah Air</td>
-                                            <td class="text-center">2 Buah</td>
-                                        </tr>
-                                        <tr>
-                                            <td class="text-center">3</td>
-                                            <td class="text-center">Drone</td>
-                                            <td class="text-center">2 Buah</td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
+                                    @endforelse
+                                </tbody>
+                            </table>
                         </div>
                     </div>
 
                     <!-- 2.4 Rekening -->
-                    <div class="row form-group data-pemilik-rekening mt-1">
+                    <div class="form-group data-pemilik-rekening">
                         <h5>Nama Rekening Pembayaran</h5>
-                        <input class="form-control mt-2" id="disabledInput" type="text" name="nama_pemilik_rekening" value="Adrian Mulyawan" disabled>
+                        <input class="form-control mt-2" id="disabledInput" type="text" name="account_owner" value="{{ $data->account_owner }}" disabled>
                     </div>
 
                     <!-- 2.5 Bukti Transfer -->
-                    <div class="row data-bukti-transfer">
+                    <div class="data-bukti-transfer">
                         <h5>Surat Pengajuan Untuk Penelitian / Pendidikan</h5>
                     </div>
-                    <div class="row mt-3">
+                    <div class="mt-3">
                         <div class="col-12">
                             <!-- 5.1 Recent 1 -->
-                            <a href="#" class="card card-list d-block">
+                            <a href="{{ Storage::url($data->evidence_of_transfer) }}" class="card card-list d-block" target="__blank">
                                 <div class="card-body">
                                     <div class="row">
                                         <div class="col-md-12">
-                                            DESC_14046.PNG
+                                            Bukti Pembayaran Retribusi
                                         </div>
                                     </div>
                                 </div>
@@ -212,18 +189,24 @@
                         </div>
                     </div>
 
-                    <!-- 2.6 Approval -->
-                    <div class="row form-group data-approval mt-4">
-                        <h5>Status Permohonan Pengajuan Izin Masuk Kawasan</h5>
-                        <select class="form-control mt-2" id="inputApproval">
-                          <option selected disabled>Ubah Status Pembayaran</option>
-                          <option>Disetujui</option>
-                        </select>
-                    </div>
-                    <div class="row justify-content-end mr-1">
-                        <a href="{{ route('AdminmanageTransaction') }}" class="btn btn-primary py-2 mr-2">Tutup</a>
-                        <button type="submit" class="btn btn-primary py-2">Ubah Status</button>
-                    </div>
+                    <form action="{{ route('AdminupdateTransactionStatus', $data->id) }}" method="post">
+                        @csrf
+                        @method('PUT')
+                        <!-- 2.6 Approval -->
+                        <div class="form-group data-approval mt-3">
+                            <h5>Status Permohonan Pengajuan Izin Masuk Kawasan</h5>
+                            <select class="form-control mt-2" id="inputApproval" name="payment_status">
+                              <option selected disabled>Ubah Status Pembayaran</option>
+                              <option value="PAIDOFF">TERBAYAR</option>
+                              <option value="FAILED">GAGAL</option>
+                            </select>
+                        </div>
+    
+                        <div class="row justify-content-end mr-3">
+                            <a href="{{ route('AdminmanageTransaction') }}" class="btn btn-primary py-2 mr-2">Tutup</a>
+                            <button type="submit" class="btn btn-primary py-2">Ubah Status</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
