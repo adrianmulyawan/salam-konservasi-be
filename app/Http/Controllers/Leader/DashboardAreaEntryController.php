a7ca5556e2bacf31c2416479df306c4db49c15e1
Adrian Mulyawan <adrianmulyawan666@gmail.com>
2022-01-27 00:32:16 +0700

    menampilkan seluruh data surat izin masuk pada halaman pimpinan

--- app/Http/Controllers/Leader/DashboardAreaEntryController.php
+++ app/Http/Controllers/Leader/DashboardAreaEntryController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\Leader;
 
 use App\Http\Controllers\Controller;
+use App\Models\Transaction;
 use Illuminate\Http\Request;
 
 class DashboardAreaEntryController extends Controller
 {
     public function index()
     {
-        return view('pages.leader.dashboard-area-entry');
+        $dataEntries = Transaction::with(['user'])->whereNotNull('entrance_ticket')->latest()->paginate(10);
+        return view('pages.leader.dashboard-area-entry', compact('dataEntries'));
     }
 }
